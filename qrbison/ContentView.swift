db29eabdb86b9091593f475908738a3fef851e88
villainwaldo <vslavov150@gmail.com>
2022-03-04 09:45:21 -0500

    Initial Commit

---
+++ qrbison/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  qrbison
+//
+//  Created by Vladimir Slavov on 3/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
