1c0a111dff3c97e2ed94c39edc176aca8171a476
obaidullahbakali <obaidullah.bakali@gallaudet.edu>
2022-03-25 11:52:27 -0400

    Reference page has been updated :)

--- qrbison/References.swift
+++ qrbison/References.swift
@@ -15,6 +15,53 @@ struct References: View {
 
 struct References_Previews: PreviewProvider {
     static var previews: some View {
-        References()
+        VStack {
+           // References()
+            Text("References")
+                .font(.system(size: 30))
+                .fontWeight(.bold)
+                .padding()
+                .foregroundColor(Color("gallyblue"))
+            Image("safercommunity")
+                .resizable()
+                .frame(width: 70, height: 70)
+            Text("Safercommunity")
+                .font(.system(size: 20))
+                .fontWeight(.bold)
+                
+                .foregroundColor(Color("gallyblue"))
+            Text("T3 Testing")
+                .font(.system(size: 15))
+                .foregroundColor(.gray)
+            
+            Image("servicenow")
+                .resizable()
+                .frame(width: 70, height: 70)
+                .padding(.top)
+            Text("ServiceNow")
+                .font(.system(size: 20))
+                .fontWeight(.bold)
+            //.padding(.top)
+                .foregroundColor(Color("gallyblue"))
+            Text("Self-assesment")
+                .font(.system(size: 15))
+                .foregroundColor(.gray)
+            
+            Image("GU")
+                .resizable()
+                .frame(width: 70, height: 70)
+                .padding(.top)
+            
+            Text("COVID Dashboard")
+                .font(.system(size: 20))
+                .fontWeight(.bold)
+                //.padding(.top)
+                .foregroundColor(Color("gallyblue"))
+            Text("Infographic")
+                .font(.system(size: 15))
+                .foregroundColor(.gray)
+            
     }
+        //.background(Color("gallycream"))
+}
 }
