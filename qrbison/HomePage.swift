e90dce9aab63d07f9e3fab48e779f594b09b3100
Kate <kate.lorenzo@gallaudet.edu>
2022-03-25 11:49:18 -0400

    header

--- qrbison/HomePage.swift
+++ qrbison/HomePage.swift
@@ -10,48 +10,51 @@ import SwiftUI
 struct HomePage: View {
     var body: some View {
         NavigationView{
-            VStack {
-          
+            ZStack {
+                VStack {
+                    Spacer()
+                    Image("gallycovid")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: Information()) {
+                        Text("Information")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .frame(maxWidth: .infinity)
+                            .padding(20)
+                            .background(Color("gallyblue"))
+                            .cornerRadius(10)
+                    }
+                    
+                    NavigationLink(destination: PersonalInformation()) {
+                        Text("Personal Information")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .frame(maxWidth: .infinity)
+                            .padding(20)
+                            .background(Color("gallyblue"))
+                            .cornerRadius(10)
+                    }
+                    
+                    NavigationLink(destination: References()) {
+                        Text("References")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .frame(maxWidth: .infinity)
+                            .padding(20)
+                            .background(Color("gallyblue"))
+                            .cornerRadius(10)
+                    }
+                    
+                    Spacer()
+                }
+                
                 TopHeader()
-                
-                Image("gallycovid")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                
-                Spacer()
-                
-                NavigationLink(destination: Information()) {
-                    Text("Information")
-                        .font(.title)
-                        .foregroundColor(Color.white)
-                        .frame(maxWidth: .infinity)
-                        .padding(20)
-                        .background(Color("gallyblue"))
-                        .cornerRadius(10)
-                }
-                
-                NavigationLink(destination: PersonalInformation()) {
-                    Text("Personal Information")
-                        .font(.title)
-                        .foregroundColor(Color.white)
-                        .frame(maxWidth: .infinity)
-                        .padding(20)
-                        .background(Color("gallyblue"))
-                        .cornerRadius(10)
-                }
-                
-                NavigationLink(destination: References()) {
-                    Text("References")
-                        .font(.title)
-                        .foregroundColor(Color.white)
-                        .frame(maxWidth: .infinity)
-                        .padding(20)
-                        .background(Color("gallyblue"))
-                        .cornerRadius(10)
-                }
-                
-                Spacer()
             }
+            
             //.padding(20)
             //}
             //.navigationTitle("Buttons")
--- qrbison/TopHeader.swift
+++ qrbison/TopHeader.swift
@@ -11,39 +11,32 @@ struct TopHeader: View {
     var body: some View {
         GeometryReader {geometry in
             ZStack{
-                ZStack {
-                    Rectangle()
-                        .frame(width: geometry.size.width * 1.4, height: geometry.size.height * 0.3)
-                                        .position(x: geometry.size.width / 2, y: geometry.size.height * 0.095)
-                        .rotationEffect(.degrees(2))
-                        .foregroundColor(Color("gallycream"))
-                        .aspectRatio(contentMode: .fit)
-                        .edgesIgnoringSafeArea(.all)
-                    
-                    Rectangle()
-                        .frame(width: geometry.size.width * 1.4, height: geometry.size.height * 0.3)
-                                        .position(x: geometry.size.width / 2, y: geometry.size.height * 0.09)
-                        .foregroundColor(Color("gallyblue"))
-                        .rotationEffect(.degrees(-5))
-                        .aspectRatio(contentMode: .fit)
-                        .edgesIgnoringSafeArea(.all)
-                }
-                
-                
                 Text("QR Bison")
-                    .frame(width: 250, height: 150)
                     .font(.system(size: 55))
                     .foregroundColor(.white)
-                    .shadow(radius: 50)
-                    .overlay(
+                    .offset(y: 45)
+            
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {}, label: {
                         Image("gallyminilogo")
                             .resizable()
-                            .frame(width: 50, height: 45, alignment: .topTrailing)
-                            .position(x: 275, y: 5)
-                    )
-                    .offset(y: -10)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50)
+                    })
+                }
+                .offset(x: -10)
             }
+            .background(
+                        Image("header")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .ignoresSafeArea()
+                    )
         }
+        .edgesIgnoringSafeArea(.all)
+        .offset(y: 0)
     }
 }
 
