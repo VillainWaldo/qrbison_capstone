e0e4b0670cdd8baae45d2b42cf4814c7c4b1c1ae
Kate <kate.lorenzo@gallaudet.edu>
2022-04-08 12:38:24 -0400

    New file for log out pop up window, added log out button on home page, added toggle function to show pop up (but button is not interactive in test)

--- qrbison/HomePage.swift
+++ qrbison/HomePage.swift
@@ -8,59 +8,73 @@
 import SwiftUI
 
 struct HomePage: View {
+    //For log out pop up window
+    @State private var showPopUp: Bool = false
+    
     var body: some View {
         NavigationView{
             ZStack {
-                VStack {
-                    Spacer()
-                    Image("gallycovid")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: Information()) {
-                        Text("Information")
-                            .font(.title)
-                            .foregroundColor(Color.white)
-                            .frame(maxWidth: .infinity)
-                            .padding(20)
-                            .background(Color("gallyblue"))
-                            .cornerRadius(10)
+                ZStack {
+                    VStack {
+                        Spacer()
+                        Image("gallycovid")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Spacer()
+                        
+                        NavigationLink(destination: Information()) {
+                            Text("Information")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(maxWidth: .infinity)
+                                .padding(20)
+                                .background(Color("gallyblue"))
+                                .cornerRadius(10)
+                        }
+                        .padding(.horizontal, 30.0)
+                        
+                        NavigationLink(destination: PersonalInformation()) {
+                            Text("Personal Information")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(maxWidth: .infinity)
+                                .padding(20)
+                                .background(Color("gallyblue"))
+                                .cornerRadius(10)
+                        }
+                        .padding(.horizontal, 30.0)
+                        
+                        NavigationLink(destination: References()) {
+                            Text("References")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(maxWidth: .infinity)
+                                .padding(20)
+                                .background(Color("gallyblue"))
+                                .cornerRadius(10)
+                        }
+                        .padding(.horizontal, 30.0)
+                        
+                        Spacer()
                     }
-                    .padding(.horizontal, 30.0)
                     
-                    NavigationLink(destination: PersonalInformation()) {
-                        Text("Personal Information")
-                            .font(.title)
-                            .foregroundColor(Color.white)
-                            .frame(maxWidth: .infinity)
-                            .padding(20)
-                            .background(Color("gallyblue"))
-                            .cornerRadius(10)
+                    TopHeader()
+                    VStack{
+                        HStack{
+                            Button{showPopUp.toggle()} label: {Image(systemName: "rectangle.portrait.and.arrow.right")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 30, height: 30)}
+                            Spacer()
+                        }
+                        Spacer()
                     }
-                    .padding(.horizontal, 30.0)
-                    
-                    NavigationLink(destination: References()) {
-                        Text("References")
-                            .font(.title)
-                            .foregroundColor(Color.white)
-                            .frame(maxWidth: .infinity)
-                            .padding(20)
-                            .background(Color("gallyblue"))
-                            .cornerRadius(10)
-                    }
-                    .padding(.horizontal, 30.0)
-                    
-                    Spacer()
+                    .padding()
+                    .offset(y: -110)
                 }
-                
-                TopHeader()
+                LogOutPop(title: "Log Out", message: "Are you sure you want to log out?", buttonText: "No", show: $showPopUp)
             }
-            
-            //.padding(20)
-            //}
-            //.navigationTitle("Buttons")
         }
     }
 }
